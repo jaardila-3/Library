2edd2a39806fc1531dd2a65708cfae8b01cf2dae
jaardila-3 <jorge.ardila1641@correo.policia.gov.co>
2021-09-09 22:44:23 -0500

    empezando a agregar inyeccion de dependencias con unity

--- WebApplication/Controllers/PruebaController.cs
+++ WebApplication/Controllers/PruebaController.cs
@@ -1,4 +1,6 @@
 ﻿using BusinessLogic;
+using BusinessLogic.Repository;
+using DataAccess.Models;
 using System.Collections.Generic;
 using System.Web.Mvc;
 
@@ -11,6 +13,18 @@ namespace WebApplication.Controllers
         {
            var lista = new prueba().ListarUsuarios();
            return View(lista);
+        }
+
+        private readonly IRepository<Usuarios> _repository;
+        public PruebaController(IRepository<Usuarios> repository)
+        {
+            _repository = repository;
+        }
+
+        public ActionResult Listar()
+        {
+            IEnumerable<Usuarios> lista = _repository.GetList();
+            return View(lista);
         }
     }
 }
